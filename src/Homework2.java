b6a9aeb246ee43b1e77569929d2a3fc19d852b92
Helena <helena.pinsel@gmail.com>
2022-09-20 16:26:03 +0300

    Swapping two variables homework

---
+++ src/Homework2.java
@@ -0,0 +1,18 @@
+/** Homework - swapping two variables
+ * @author helena
+ */
+public class Homework2 {
+    public static void main(String[] args) {
+
+        String a = "Cat";
+        String b = "Frog";
+        String temp;
+
+        temp = a;
+        a = b;
+        b = temp;
+
+        System.out.println("a: "+a);
+        System.out.println("b: "+b);
+    }
+}
